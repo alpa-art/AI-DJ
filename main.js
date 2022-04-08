aa5c9db5c719cc2b002666bb01779abe9fdd5b8e
alpa-art <84176087+alpa-art@users.noreply.github.com>
2022-04-08 16:41:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+sound="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+function preload()
+{
+    song=loadSound("music.mp3");
+}
+
+function setup()
+{
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+video.hide();
+
+poseNet = ml5.poseNet( video, modalLoaded );
+PoseNet.on('pose', gotPoses );
+}
+function modalLoaded()
+{
+    console.log('PoseNet is Initialized');
+}
+function gotPoses(results)
+{
+    if(results.length  >  0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.X;
+        leftWristY = results[0].pose.leftWrist.Y;
+       
+        rightWristX = results[0].pose.rightWrist.X;
+        rightWristY= results[0].pose.rightWrist.Y;
+    }
+}
+function draw()
+{
+    image(video,0,0,600,500);
+}
+
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
